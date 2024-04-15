46078c494950bf5c0b92670d26f6f5a0ab21cbf6
asago92 <101930040+asago92@users.noreply.github.com>
2024-04-15 16:07:53 +0200

    Update 15history.py

--- 15history.py
+++ 15history.py
@@ -25,4 +25,4 @@ chart = alt.Chart(source).mark_area().encode(
 st.title("Visualizing 40 Years of Music Industry Sales")
 st.write("The data reflects the evolution in music consumption, shifting from physical media like CDs and cassettes to digital downloads and streaming services. This transition highlights the music industry's adaptation to technological advancements and changing consumer preferences.")
 
-st.altair_chart(chart, theme="streamlit", use_container_width=True)
+st.altair_chart(chart, theme=None, use_container_width=True)
