786b29ec343e311adf73766b695d918bce807276
asago92 <101930040+asago92@users.noreply.github.com>
2024-04-15 17:05:11 +0200

    Update 15history.py

--- 15history.py
+++ 15history.py
@@ -8,21 +8,23 @@ st.set_page_config(
     page_icon="📊",
     layout="wide"
 )
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["Comparisons", "Distributions", "Relationships", "Timeseries", "Uncertainties"])
 
-source = pd.read_excel('MusicData.xlsx')
-source['Year'] = pd.to_datetime(source['Year'], format='%Y')
-
-chart = alt.Chart(source).mark_area().encode(
-    alt.X('Year:T',
-        axis=alt.Axis(format='%Y', domain=False, tickSize=0)
-    ),
-    alt.Y('Value (Actual):Q', aggregate='sum', stack='center', axis=None),
-    alt.Color('Format:N',
-        scale=alt.Scale(scheme='tableau20')
-    )
-).interactive()
-
-st.title("Visualizing 40 Years of Music Industry Sales")
-st.write("The data reflects the evolution in music consumption, shifting from physical media like CDs and cassettes to digital downloads and streaming services. This transition highlights the music industry's adaptation to technological advancements and changing consumer preferences.")
-
-st.altair_chart(chart, theme="streamlit", use_container_width=True)
+with tab3:
+    source = pd.read_excel('MusicData.xlsx')
+    source['Year'] = pd.to_datetime(source['Year'], format='%Y')
+    
+    chart = alt.Chart(source).mark_area().encode(
+        alt.X('Year:T',
+            axis=alt.Axis(format='%Y', domain=False, tickSize=0)
+        ),
+        alt.Y('Value (Actual):Q', aggregate='sum', stack='center', axis=None),
+        alt.Color('Format:N',
+            scale=alt.Scale(scheme='tableau20')
+        )
+    ).interactive()
+    
+    st.title("Visualizing 40 Years of Music Industry Sales")
+    st.write("The data reflects the evolution in music consumption, shifting from physical media like CDs and cassettes to digital downloads and streaming services. This transition highlights the music industry's adaptation to technological advancements and changing consumer preferences.")
+    
+    st.altair_chart(chart, theme="streamlit", use_container_width=True)
