df98a04ddf2abed63ac4c2e6719ded208b3afb1b
asago92 <101930040+asago92@users.noreply.github.com>
2024-04-15 15:44:18 +0200

    Update 15history.py

--- 15history.py
+++ 15history.py
@@ -15,4 +15,4 @@ chart = alt.Chart(source).mark_area().encode(
     )
 ).interactive()
 
-st.altair_chart(chart, theme="streamlit", use_container_width=True)
+st.altair_chart(chart, theme="None", use_container_width=True)
