288e7c3c2737d05f2c2327859999e3b52b0c118f
asago92 <101930040+asago92@users.noreply.github.com>
2024-04-16 16:53:14 +0200

    Update 15history.py

--- 15history.py
+++ 15history.py
@@ -83,7 +83,7 @@ with tab3:
         alt.Fill(
             'mean_temp:Q',
             legend=None,
-            scale=alt.Scale(domain=[-30, 15], scheme='redyellowblue')
+            scale=alt.Scale(domain=[-30, 15], scheme='yellowgreenblue')
         )
     ).facet(
         row=alt.Row(
